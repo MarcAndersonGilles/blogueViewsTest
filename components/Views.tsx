0adf495ed9bbfa416fcdda1b6b29e3b71c8c9db5
marc <hornil6748@gmail.com>
2024-01-12 11:27:47 -0500

    feature/ added supabase admin

--- components/Views.tsx
+++ components/Views.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { supabase } from '@/supabase/supabaseClient'
+import React, {useEffect, useState} from 'react'
 
 function Views() {
+  const [supabaseData, setSupabaseData] = useState<any[]>([])
+
+  const testSupabase = async () => {
+    const { data, error } = await supabase
+      .from('pages')
+      .select('*')
+    console.log(data)
+    if(error){
+      console.log(error)
+    }
+    if(data){
+      setSupabaseData(data)
+    }
+   
+  }
+  useEffect(() => {
+    testSupabase()
+  },[])
   return (
     <div>Blogue</div>
   )
