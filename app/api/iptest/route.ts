6f9d1244c791e023eec0843999d98bcb25aea3f9
marc <hornil6748@gmail.com>
2024-02-10 11:11:16 -0500

    make ip works again testing

--- app/api/iptest/route.ts
+++ app/api/iptest/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse,NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
-    // return NextResponse.json({ ip: req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for')  }, { status: 200 });
-    return NextResponse.json({ ip: req.ip  }, { status: 200 });
+    return NextResponse.json({ ip: req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for')  }, { status: 200 });
 }
