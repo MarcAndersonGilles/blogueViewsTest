072b931ceba806d88ab69dabdb242be1f078e758
marc <hornil6748@gmail.com>
2024-02-10 11:05:52 -0500

    testing

--- app/api/iptest/route.ts
+++ app/api/iptest/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse,NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
-    return NextResponse.json({ ip: req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for')  }, { status: 200 });
+    // return NextResponse.json({ ip: req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for')  }, { status: 200 });
+    return NextResponse.json({ ip: req.ip  }, { status: 200 });
 }
